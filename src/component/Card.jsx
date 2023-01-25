3c198136a1eb5c7502b77d78dbccda9ee2862b93
Saad Awan <saad03112503171@gmail.com>
2023-01-25 23:38:04 +0500

    edit

---
+++ src/component/Card.jsx
@@ -0,0 +1,83 @@
+import React from 'react'
+
+
+function Card() {
+  return (
+    <div className=''>
+    <div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/01/SwatValleyPakistanSAKhanPhotographyCanvaPro.jpg" style={{borderRadius:"10px" }} class="card-img-top" alt="..."/>
+  <div class="card-body">
+    <h5 class="card-title">SWAT VALLEY</h5>
+    <p class="card-text">This stunning valley in the Khyber Pakhtunkhwa province of Pakistan is something straight out of a fairy tale.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/12/Beautiful-places-in-Pakistan-Hunza-Valley-undefinedGetty-CanvaPro.jpg" class="card-img-top" style={{height:"12rem",borderRadius:"10px" }} alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">HUNZA VALLEY</h5>
+    <p class="card-text">If you live in Pakistan – or have read anything about the country – it’s almost certain you’ve come across the name Hunza</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/01/YarkhunValleySamanthaIntentionalDetours.jpg" style={{height:"12.3rem",borderRadius:"10px" }} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">YARKHUN VALLEY</h5>
+    <p class="card-text"> Located in the Upper Chitral district of Khyber Pakhtunkhwa, Yarkhun dazzles with its mountain ranges and untouched villages.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/01/PhanderLakePakistanKanokwanPonokCanvaPro.jpg" style={{height:"12.3rem",borderRadius:"10px" }} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">PHANDER LAKE</h5>
+    <p class="card-text">Despite being insanely beautiful, Phander Lake doesn’t see anywhere as close to the number of tourists as the more popular Attabad Lake does.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/01/LahorePakistanHomoCosmicosCanvaPro.jpg" style={{height:"12.3rem",borderRadius:"10px" }} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">LAHORE</h5>
+    <p class="card-text">A city… Say what? Yes, Lahore may be a metro but its treasure trove of historical places surely makes it one of the most beautiful places to visit in Pakistan.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/01/Beautiful-places-in-Pakistan-Hingol-National-Park-LukasBischoffGetty-CanvaPro.jpg" style={{height:"12.3rem",borderRadius:"10px" }} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">HINGOL NATIONAL PARK</h5>
+    <p class="card-text">What’s more, part of the National Park hugs the coast, adding the ocean to all its other assets. Though totally out-of-this-world in its looks, Hingol is only 3.5 hours </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/01/BamboretValleyPakistanPawopa3336CanvaPro.jpg" style={{height:"12.3rem",borderRadius:"10px" }} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">KALASH VALLEYS</h5>
+    <p class="card-text">The Kalash Valleys, comprised of Bumboret, Rumbur, and Birir, are home to the Kalash people, a religious and ethnic minority in Pakistan with their own beliefs.</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/01/Beautiful-places-in-Pakistan-Deosai-ShahsoftGetty-CanvaPro.jpg" class="card-img-top" style={{height:"12.3rem",borderRadius:"10px" }} alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">DEOSAI PLAINS NATIONAL PARK</h5>
+    <p class="card-text">Deosai is often referred to as the roof of the world. And it kinda is. At 4,117 metres (13,497 feet), the massive plateau is the sec-heightest</p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+<div class="card my-3" style={{width: "18rem",display:"inline-block",marginLeft:"65px",borderRadius:"10px"}}>
+  <img src="https://wander-lush.org/wp-content/uploads/2020/01/ShahJahanMosqueMuhammadFarooqCanvaPro.jpg" style={{height:"12.3rem",borderRadius:"10px" }} class="card-img-top" alt="..." />
+  <div class="card-body">
+    <h5 class="card-title">SHAH JAHAN MOSQUE</h5>
+    <p class="card-text"> It’s widely known for having the most elaborate display of tile work in all of South Asia. Blue and sandstone colours adorn the interior of the mosque and are sure </p>
+    <a href="#" class="btn btn-primary">Go somewhere</a>
+  </div>
+</div>
+</div >
+  )
+}
+
+export default Card
---
+++ src/component/Hero.jsx
@@ -0,0 +1,32 @@
+function Hero(){
+    return(
+        <div id="carouselExampleIndicators" class="carousel slide" data-bs-ride="carousel">
+  <div class="carousel-indicators">
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="1" aria-label="Slide 2"></button>
+    <button type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide-to="2" aria-label="Slide 3"></button>
+  </div>
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="https://cdn.pixabay.com/photo/2015/04/23/22/00/tree-736885_960_720.jpg" class="d-block w-100"  alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="https://media.istockphoto.com/id/517188688/photo/mountain-landscape.jpg?s=612x612&w=0&k=20&c=A63koPKaCyIwQWOTFBRWXj_PwCrR4cEoOw2S9Q7yVl8=" class="d-block w-100" alt="..." />
+    </div>
+    <div class="carousel-item">
+      <img src="https://thumbs.dreamstime.com/b/environment-earth-day-hands-trees-growing-seedlings-bokeh-green-background-female-hand-holding-tree-nature-field-gra-130247647.jpg" class="d-block w-100" alt="..." />
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleIndicators" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+    );
+}
+
+export default Hero
